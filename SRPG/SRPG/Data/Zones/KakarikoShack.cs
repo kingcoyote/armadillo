e16e97168602df9d4e388948589a3379a96b6976
Steve Phillips <kingcoyote85@gmail.com>
2013-06-09 14:59:50 -0700

    added a shack / bomb shop to kakariko village to test the zone changing functionality

--- SRPG/SRPG/Data/Zones/KakarikoShack.cs
+++ SRPG/SRPG/Data/Zones/KakarikoShack.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Torch;
+using Game = Torch.Game;
 
 namespace SRPG.Data.Zones
 {
@@ -9,7 +13,19 @@ namespace SRPG.Data.Zones
     {
         public KakarikoShack()
         {
-            
+            Name = "Kakariko Shop";
+            Sandbag = Grid.FromBitmap("kakariko_shop_sandbag");
+            ImageLayers = new List<ImageObject>
+                {
+                    new ImageObject(Game.GetInstance().Content.Load<Texture2D>("kakariko_shop")) {Z = -1},
+                };
+
+            Objects = new List<InteractiveObject>();
+
+            Doors = new List<Door>
+                {
+                    new Door { Location = new Rectangle(264, 457, 48, 14), Name = "entrance", Orientation = Direction.Up, Zone = "kakariko", ZoneDoor = "shack" }
+                };
         }
     }
 }
