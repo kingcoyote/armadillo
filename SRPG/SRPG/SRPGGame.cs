5b4d3c3b70fdab868a03c4d06ff9ccad51e766c2
Steve <visual77@gmail.com>
2013-06-14 13:43:06 -0700

    cleaned up the main file a little

--- SRPG/SRPG/SRPGGame.cs
+++ SRPG/SRPG/SRPGGame.cs
@@ -1,22 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using SRPG.Data;
-using SRPG.Data.Items;
 using SRPG.Scene.Battle;
 using SRPG.Scene.Intro;
 using SRPG.Scene.PartyMenu;
 using SRPG.Scene.Options;
 using SRPG.Scene.Overworld;
 using SRPG.Scene.Shop;
-using Torch;
 
 namespace SRPG
 {
@@ -43,13 +33,8 @@ namespace SRPG
             Scenes.Add("options", new OptionsScene(this));
 
             CurrentScene = "intro";
-        }
 
-        protected override void Initialize()
-        {
-            base.Initialize();
-
-            
+            Item.InitializeItemList();
         }
 
         public void StartGame()
