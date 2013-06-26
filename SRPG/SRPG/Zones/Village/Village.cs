d072cfd088f56fe6f35dc1ec33c77d76cbe2b220
Steve Phillips <kingcoyote85@gmail.com>
2013-06-25 21:36:17 -0700

    updated the village with the new artwork, and added a .psd file that contains the current fully rendered village as a reference

--- SRPG/SRPG/Zones/Village/Village.cs
+++ SRPG/SRPG/Zones/Village/Village.cs
@@ -19,9 +19,13 @@ namespace SRPG.Zones.Village
             
             ImageLayers.Add(new ImageObject(Game.GetInstance().Content.Load<Texture2D>("Zones/Village/Village/village")));
 
+            ImageLayers.Add(new ImageObject(Game.GetInstance().Content.Load<Texture2D>("Zones/Village/Village/inn")) { X = 840, Y = 1372, Z = 1752 });
+            ImageLayers.Add(new ImageObject(Game.GetInstance().Content.Load<Texture2D>("Zones/Village/Village/warehouse")) { X = 155, Y = 1300, Z = 1918 });
+            ImageLayers.Add(new ImageObject(Game.GetInstance().Content.Load<Texture2D>("Zones/Village/Village/well")) { X = 1400, Y = 863, Z = 977 });
+
             Doors.Add(new Door { Location = new Rectangle(1442, 2057, 171, 37), Name = "coliseum", Orientation = Direction.Up, Zone = "coliseum/halls", ZoneDoor = "village" });
-            Doors.Add(new Door { Location = new Rectangle(69*6, 315*6, 9*6, 1), Name = "warehouse", Orientation = Direction.Down, Zone = "village/warehouse", ZoneDoor = "entrance" });
-            Doors.Add(new Door { Location = new Rectangle(69 * 6, 318 * 6, 9 * 6, 1), Name = "warehouse exit", Orientation = Direction.Down });
+            Doors.Add(new Door { Location = new Rectangle(69*6, 315*6, 9*6, 2), Name = "warehouse", Orientation = Direction.Down, Zone = "village/warehouse", ZoneDoor = "entrance" });
+            Doors.Add(new Door { Location = new Rectangle(69*6, 320*6, 9*6, 2), Name = "warehouse exit", Orientation = Direction.Down });
         }
     }
 }
