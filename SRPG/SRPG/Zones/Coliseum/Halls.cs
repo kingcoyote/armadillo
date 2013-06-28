80eb4b2df0660b46afe11b1ae6a6dc2b38c3b498
Steve Phillips <kingcoyote85@gmail.com>
2013-06-27 22:57:46 -0700

    updated the coliseum/halls zone images. the main image itself was not perfectly lined up to a 50x50 grid, which it needs to be for the battle to take place on it. the round room on the right is okay, but there were two walls in the northwest room that were off by enough to cause issues. i also created the battle grid and moved the doors to line up with the new image

--- SRPG/SRPG/Zones/Coliseum/Halls.cs
+++ SRPG/SRPG/Zones/Coliseum/Halls.cs
@@ -18,13 +18,13 @@ namespace SRPG.Zones.Coliseum
             Sandbag = Grid.FromBitmap("Zones/Coliseum/Halls/sandbag");
             ImageLayers.Add(new ImageObject(Game.GetInstance().Content.Load<Texture2D>("Zones/Coliseum/Halls/halls")));
 
-            Doors.Add(new Door {Location = new Rectangle(734, 1933, 55, 8), Name = "cell", Orientation = Direction.Up });
-            Doors.Add(new Door { Location = new Rectangle(487, 160, 51, 12), Name = "village", Orientation = Direction.Down });
+            Doors.Add(new Door {Location = new Rectangle(691, 1847, 67, 43), Name = "cell", Orientation = Direction.Up });
+            Doors.Add(new Door { Location = new Rectangle(446, 108, 57, 31), Name = "village", Orientation = Direction.Down });
 
-            Objects.Add(new InteractiveObject{ Interact = SimpleDoor("coliseum/cell", "halls"), Location = new Rectangle(733, 1945, 60, 11) });
-            Objects.Add(new InteractiveObject { Interact = SimpleDoor("village/village", "coliseum"), Location = new Rectangle(487, 138, 51, 12) });
+            Objects.Add(new InteractiveObject{ Interact = SimpleDoor("coliseum/cell", "halls"), Location = new Rectangle(691, 1895, 67, 12) });
+            Objects.Add(new InteractiveObject { Interact = SimpleDoor("village/village", "coliseum"), Location = new Rectangle(446, 90, 57, 12) });
 
-            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/hall"), Location = new Rectangle(1875, 1120, 31, 64) });
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/hall"), Location = new Rectangle(1839, 1070, 16, 65) });
         }
     }
 }
