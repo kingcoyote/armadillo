c1f33d98e67da58f0839abdced57cf0c1f4641b5
Steve Phillips <kingcoyote85@gmail.com>
2013-07-04 10:47:44 -0700

    added a keybinding in the options menu to go back to the overworld

--- SRPG/SRPG/Scene/Options/OptionsScene.cs
+++ SRPG/SRPG/Scene/Options/OptionsScene.cs
@@ -1,9 +1,21 @@
-﻿using Torch;
+﻿using Microsoft.Xna.Framework.Input;
+using Torch;
 
 namespace SRPG.Scene.Options
 {
     class OptionsScene : Torch.Scene
     {
-        public OptionsScene(Game game) : base(game) { }
+        public OptionsScene(Game game) : base(game)
+        {
+            var keyboard = new KeyboardInputLayer(this);
+            keyboard.AddKeyDownBinding(Keys.Escape, () => Game.GetInstance().ChangeScenes("overworld"));
+
+            Layers.Add("keyboard", keyboard);
+        }
+
+        public override void Initialize()
+        {
+            
+        }
     }
 }
