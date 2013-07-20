db5cab871029fe35fa80235c2e9b852e50d08a29
Steve Phillips <kingcoyote85@gmail.com>
2013-07-20 12:16:13 -0700

    added a TorchHelper object that, for right now, just houses a quick function to convert a Vector2 into a Point

---
+++ Torch/TorchHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Torch
+{
+    public class TorchHelper
+    {
+        public static Point Vector2ToPoint(Vector2 vector)
+        {
+            return new Point((int)vector.X, (int)vector.Y);
+        }
+    }
+}
