9ce7b473f85c3f9b67a4e1b9e4dee5ffab32f9b3
Steve Phillips <kingcoyote85@gmail.com>
2013-09-13 16:39:16 -0700

    bound functionality to the main menu buttons new/exit buttons

--- SRPG/SRPG/Scene/MainMenu/MainMenu.cs
+++ SRPG/SRPG/Scene/MainMenu/MainMenu.cs
@@ -9,15 +9,20 @@ namespace SRPG.Scene.MainMenu
 {
     class MainMenu : Torch.Scene
     {
+        private MenuOptionsDialog _menuOptionsDialog;
         public MainMenu(Game game) : base(game) { }
     
         public override void Initialize()
         {
             base.Initialize();
 
-
             Components.Add(new BackgroundLayer(this, "MainMenu/bg") { DrawOrder = -10000 });
-            Gui.Screen.Desktop.Children.Add(new MenuOptionsDialog());
+            _menuOptionsDialog = new MenuOptionsDialog();
+
+            _menuOptionsDialog.OnExitPressed += (s, a) => Game.Exit();
+            _menuOptionsDialog.OnNewGamePressed += (s, a) => ((SRPGGame) Game).StartGame();
+
+            Gui.Screen.Desktop.Children.Add(_menuOptionsDialog);
             Gui.Visualizer = FlatGuiVisualizer.FromFile(Game.Services, "Content/Gui/main_menu.xml");
         }
 
--- SRPG/SRPG/Scene/MainMenu/MenuOptionsDialog.cs
+++ SRPG/SRPG/Scene/MainMenu/MenuOptionsDialog.cs
@@ -8,9 +8,21 @@ namespace SRPG.Scene.MainMenu
 {
     public partial class MenuOptionsDialog
     {
+        public EventHandler OnNewGamePressed;
+        public EventHandler OnContinuePressed;
+        public EventHandler OnLoadGamePressed;
+        public EventHandler OnOptionsPressed;
+        public EventHandler OnExitPressed;
+
         public MenuOptionsDialog()
         {
             InitializeComponent();
+
+            _newGame.Pressed += (s, a) => OnNewGamePressed.Invoke(s, a);
+            _continue.Pressed += (s, a) => OnContinuePressed.Invoke(s, a);
+            _loadGame.Pressed += (s, a) => OnLoadGamePressed.Invoke(s, a);
+            _options.Pressed += (s, a) => OnOptionsPressed.Invoke(s, a);
+            _exit.Pressed += (s, a) => OnExitPressed.Invoke(s, a);
         }
     }
 }
