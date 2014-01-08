bd266e49d525294c7753ea0c0967ad3b81166e6f
Steve Phillips <kingcoyote85@gmail.com>
2014-01-07 20:51:28 -0800

    put in stats that more closely resemble the final stats

--- SRPG/SRPGContent/Battle/Combatants/party.js
+++ SRPG/SRPGContent/Battle/Combatants/party.js
@@ -2,17 +2,25 @@
   jaha : {
     class : "Fighter",
     avatar : "fighter",
-    health : 20,
-    mana : 20,
+    health : 40,
+    mana : 8,
     weapontype : "sword",
     armortype : "plate",
     stats : {
-      defense : 15,
-      attack : 15,
-      wisdom : 15,
-      intelligence : 15,
-      speed : 15,
-      hit : 15
+      defense : 30,
+      attack : 20,
+      wisdom : 10,
+      intelligence : 10,
+      speed : 5,
+      hit : 5
+    },
+    statxp : {
+      defense : 325,
+      attack : 265,
+      wisdom : 110,
+      intelligence : 130,
+      speed : 50,
+      hit : 75
     },
     abilities : [
     
@@ -27,17 +35,25 @@
   meera : {
     class : "Ranger",
     avatar : "ranger",
-    health : 20,
-    mana : 20,
+    health : 30,
+    mana : 15,
     weapontype : "gun",
     armortype : "mail",
     stats : {
-      defense : 15,
-      attack : 15,
-      wisdom : 15,
-      intelligence : 15,
-      speed : 15,
-      hit : 15
+      defense : 10,
+      attack : 10,
+      wisdom : 5,
+      intelligence : 5,
+      speed : 30,
+      hit : 20
+    },
+    statxp : {
+      defense : 110,
+      attack : 130,
+      wisdom : 50,
+      intelligence : 75,
+      speed : 325,
+      hit : 265
     },
     abilities : [
       
@@ -52,17 +68,25 @@
   gratho : {
     class : "Monk",
     avatar : "monk",
-    health : 20,
-    mana : 20,
+    health : 25,
+    mana : 18,
     weapontype : "unarmed",
     armortype : "leather",
     stats : {
-      defense : 15,
-      attack : 15,
-      wisdom : 15,
-      intelligence : 15,
-      speed : 15,
-      hit : 15
+      defense : 5,
+      attack : 10,
+      wisdom : 10,
+      intelligence : 5,
+      speed : 20,
+      hit : 30
+    },
+    statxp : {
+      defense : 75,
+      attack : 130,
+      wisdom : 110,
+      intelligence : 50,
+      speed : 265,
+      hit : 325
     },
     abilities : [
       
@@ -77,17 +101,25 @@
   aeris : {
     class : "Cleric",
     avatar : "cleric",
-    health : 20,
-    mana : 20,
+    health : 16,
+    mana : 30,
     weapontype : "book",
     armortype : "cloth",
     stats : {
-      defense : 15,
-      attack : 15,
-      wisdom : 15,
-      intelligence : 15,
-      speed : 15,
-      hit : 15
+      defense : 5,
+      attack : 5,
+      wisdom : 30,
+      intelligence : 20,
+      speed : 10,
+      hit : 10
+    },
+    statxp : {
+      defense : 75,
+      attack : 50,
+      wisdom : 325,
+      intelligence : 265,
+      speed : 130,
+      hit : 110
     },
     abilities : [
       
@@ -102,17 +134,25 @@
   sheena : {
     class : "Wizard",
     avatar : "wizard",
-    health : 20,
-    mana : 20,
+    health : 12,
+    mana : 40,
     weapontype : "staff",
     armortype : "cloth",
     stats : {
-      defense : 15,
-      attack : 15,
-      wisdom : 15,
-      intelligence : 15,
-      speed : 15,
-      hit : 15
+      defense : 5,
+      attack : 5,
+      wisdom : 10,
+      intelligence : 30,
+      speed : 10,
+      hit : 20
+    },
+    statxp : {
+      defense : 50,
+      attack : 75,
+      wisdom : 130,
+      intelligence : 325,
+      speed : 110,
+      hit : 265
     },
     abilities : [
       { name: "quake", experience: 115 }
