a29b433d124f274b0e01425a27a7197591a26cec
Steve Phillips <kingcoyote85@gmail.com>
2014-01-09 10:26:30 -0800

    added delegates to bubble up the request to change a character's item

--- SRPG/SRPG/Scene/PartyMenu/CharacterInfoDialog.cs
+++ SRPG/SRPG/Scene/PartyMenu/CharacterInfoDialog.cs
@@ -5,16 +5,27 @@ namespace SRPG.Scene.PartyMenu
 {
     partial class CharacterInfoDialog : WindowControl
     {
+        private Combatant _character;
+
+        public delegate void ChangeItemDelegate(Combatant character, ItemEquipType type);
+        public ChangeItemDelegate ChangeItem = (c, t) => { };
+
         public CharacterInfoDialog()
         {
             InitializeComponent();
+
+            _inventoryDialog.WeaponChange += () => ChangeItem.Invoke(_character, ItemEquipType.Weapon);
+            _inventoryDialog.ArmorChange += () => ChangeItem.Invoke(_character, ItemEquipType.Armor);
+            _inventoryDialog.AccessoryChange += () => ChangeItem.Invoke(_character, ItemEquipType.Accessory);
         }
 
         public void SetCharacter(Combatant character)
         {
-            _overviewDialog.UpdateCharacter(character);
-            _statsDialog.UpdateCharacter(character);
-            _inventoryDialog.UpdateCharacter(character);
+            _character = character;
+
+            _overviewDialog.UpdateCharacter(_character);
+            _statsDialog.UpdateCharacter(_character);
+            _inventoryDialog.UpdateCharacter(_character);
         }
     }
 }
