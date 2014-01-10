23b6c7fa1630c4a00fe5ecd88ffc4ce5dfdbba95
Steve Phillips <kingcoyote85@gmail.com>
2014-01-10 13:44:53 -0800

    added interaction to the pillars in the north hall that launches the test battle.

--- SRPG/SRPG/Zones/Coliseum/HallsNorth.cs
+++ SRPG/SRPG/Zones/Coliseum/HallsNorth.cs
@@ -2,6 +2,7 @@
 using SRPG.Data;
 using Torch;
 using Game = Microsoft.Xna.Framework.Game;
+using Object = Torch.Object;
 
 namespace SRPG.Zones.Coliseum
 {
@@ -24,6 +25,14 @@ namespace SRPG.Zones.Coliseum
 
             Objects.Add(new InteractiveObject { Interact = SimpleDoor("coliseum/halls-south", "halls-north"), Location = new Rectangle(500, 740, 50, 15) });
             Objects.Add(new InteractiveObject { Interact = SimpleDoor("village/village", "coliseum"), Location = new Rectangle(455, 90, 140, 15) });
+
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/halls"), Location = new Rectangle(400, 200, 50, 50) });
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/halls"), Location = new Rectangle(400, 400, 50, 50) });
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/halls"), Location = new Rectangle(400, 600, 50, 50) });
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/halls"), Location = new Rectangle(600, 200, 50, 50) });
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/halls"), Location = new Rectangle(600, 400, 50, 50) });
+            Objects.Add(new InteractiveObject { Interact = TestBattle("coliseum/halls"), Location = new Rectangle(600, 600, 50, 50) });
+
         }
     }
 }
