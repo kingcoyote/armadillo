401030dcb7ec2edcf6e1210811585b9ffd1839ca
Steve Phillips <kingcoyote85@gmail.com>
2014-01-12 11:49:48 -0800

    renamed InventoryWindowControl to InventoryDialog

---
+++ SRPG/SRPG/Scene/PartyMenu/InventoryDialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SRPG.Data;
+
+namespace SRPG.Scene.PartyMenu
+{
+    public partial class InventoryDialog
+    {
+        public InventoryDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void SetInventory(IEnumerable<Item> items)
+        {
+
+        }
+    }
+}
---
+++ SRPG/SRPG/Scene/PartyMenu/InventoryDialog.designer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Torch.UserInterface;
+using Nuclex.UserInterface;
+
+namespace SRPG.Scene.PartyMenu
+{
+    public partial class InventoryDialog : WindowControl
+    {
+        private void InitializeComponent()
+        {
+            Bounds = new UniRectangle(
+                new UniScalar(0.0f, 0.0f), new UniScalar(0.0f, 105.0f),
+                new UniScalar(0.2f, 0.0f), new UniScalar(1.0f, -105.0f)
+            );
+            EnableDragging = false;
+        }
+    }
+}
