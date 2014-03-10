d8e02c9cd2e4609eddc8099ce8e5e3a2f02cdb1d
Steve Phillips <kingcoyote85@gmail.com>
2014-03-10 13:47:46 -0700

    added placeholder elements for the shop

--- SRPG/SRPG/Scene/Shop/ShopScene.cs
+++ SRPG/SRPG/Scene/Shop/ShopScene.cs
@@ -15,6 +15,15 @@ namespace SRPG.Scene.Shop
     {
         private List<Item> _inventory = new List<Item>();
 
+        private InventoryDialog _shopInventory;
+        private InventoryDialog _playerInventory;
+        private InfoDialog _infoDialog;
+        private PartyDialog _partyDialog;
+
+        private ButtonControl _exitButton;
+        private ButtonControl _buyButton;
+        private ButtonControl _sellButton;
+
         public ShopScene(Game game) : base(game)
         {
             var keyboard = new KeyboardInputLayer(this, null);
@@ -27,17 +36,63 @@ namespace SRPG.Scene.Shop
             Gui.Visualizer = FlatGuiVisualizer.FromFile(Game.Services, "Content/Gui/main_gui.xml");
 
             // GUI elements:
-            // shop inventory
-            // player inventory
-            // exit button
-            // money
+            _shopInventory = new InventoryDialog();
+            _shopInventory.Bounds = new UniRectangle(
+                new UniScalar(0), new UniScalar(0),
+                new UniScalar(1.0F, -160), new UniScalar(0.3F, -27.5F) 
+            );
+            Gui.Screen.Desktop.Children.Add(_shopInventory);
+
+            _playerInventory = new InventoryDialog();
+            _playerInventory.Bounds = new UniRectangle(
+                new UniScalar(0), new UniScalar(0.3F, 27.5F),
+                new UniScalar(1.0F, -160), new UniScalar(0.3F, -27.5F) 
+            );
+            Gui.Screen.Desktop.Children.Add(_playerInventory);
+
+            // money / information
+            _infoDialog = new InfoDialog();
+            _infoDialog.Bounds = new UniRectangle(
+                new UniScalar(1.0F, -150), new UniScalar(0.0F, 45),
+                new UniScalar(150), new UniScalar(0.6F, -45)
+            );
+            Gui.Screen.Desktop.Children.Add(_infoDialog);
+            
             // party
+            _partyDialog = new PartyDialog();
+            _partyDialog.Bounds = new UniRectangle(
+                new UniScalar(0), new UniScalar(0.6F, 10),
+                new UniScalar(1.0F, 0), new UniScalar(0.4F, -10)
+            );
+            Gui.Screen.Desktop.Children.Add(_partyDialog);
+
+            // exit button
+            _exitButton = new ButtonControl();
+            _exitButton.Text = "Exit";
+            _exitButton.Bounds = new UniRectangle(
+                new UniScalar(1.0F, -150), new UniScalar(0),
+                new UniScalar(150), new UniScalar(35)
+            );
+            _exitButton.Pressed += (s, a) => Game.PopScene();
+            Gui.Screen.Desktop.Children.Add(_exitButton);
 
             // buy selected
-            // sell selected
+            _buyButton = new ButtonControl();
+            _buyButton.Text = "Buy";
+            _buyButton.Bounds = new UniRectangle(
+                new UniScalar(0), new UniScalar(0.3F, -17.5F),
+                new UniScalar(150), new UniScalar(35)
+            );
+            Gui.Screen.Desktop.Children.Add(_buyButton);
 
-            // try on
-            // buy and equip
+            // sell selected
+            _sellButton = new ButtonControl();
+            _sellButton.Text = "Sell";
+            _sellButton.Bounds = new UniRectangle(
+                new UniScalar(1.0F, -310), new UniScalar(0.3F, -17.5F),
+                new UniScalar(150), new UniScalar(35)
+            );
+            Gui.Screen.Desktop.Children.Add(_sellButton);
         }
 
         protected override void OnEntered()
